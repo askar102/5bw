07214f84315c94ef94bc59a65f55ba71d97ac6fd
askar102 <sergey.bolotny@icloud.com>
2026-04-29 18:06:25 +0500

    some refactoring

--- battle/abilityManager.cpp
+++ battle/abilityManager.cpp
@@ -1,43 +1,42 @@
 #include "abilityManager.h"
 
 namespace AbilityManager {
-namespace CardGuy {
+    namespace CardGuy {
+        void SpawnCardAttack(BattleResources& resManager, VfxManager& vfxManager, BattleEntity& caster, BattleEntity& target, int damage)
+        {
+            Vector2 casterPos = caster.getSprite().getPosition();
+            Vector2 cardPostion = {
+                casterPos.x + 40,
+                casterPos.y + (caster.getSprite().getSize().y / 2)
+            };
 
-void SpawnCardAttack(BattleResources& resManager, VfxManager& vfxManager, BattleEntity& caster, BattleEntity& target, int damage)
-{
-    Vector2 casterPos = caster.getSprite().getPosition();
-    Vector2 cardPostion = {
-        casterPos.x + 40,
-        casterPos.y + (caster.getSprite().getSize().y / 2)
-    };
+            caster.getSprite().setTexture(resManager.CardGuyAtlasTexture());
+            caster.getSprite().setCostume(1, 1.0f); // second costume from 3-part atlas
 
-    caster.getSprite().setTexture(resManager.CardGuyAtlasTexture());
-    caster.getSprite().setCostume(1, 1.0f); // second costume from 3-part atlas
+            // up
+            vfxManager.SpawnCardVfx(cardPostion, 5.0f, -10.0f, target, false);
 
-    // up
-    vfxManager.SpawnCardVfx(cardPostion, 5.0f, -10.0f, target, false);
+            // mid
+            vfxManager.SpawnCardVfx(cardPostion, 5.0f, 0.0f, target, false);
 
-    // mid
-    vfxManager.SpawnCardVfx(cardPostion, 5.0f, 0.0f, target, false);
+            // down
+            vfxManager.SpawnCardVfx(cardPostion, 5.0f, 10.0f, target, false);
+        }
 
-    // down
-    vfxManager.SpawnCardVfx(cardPostion, 5.0f, 10.0f, target, false);
-}
+        void SpawnCardHeal(BattleResources& resManager, VfxManager& vfxManager, BattleEntity& caster, BattleEntity& target)
+        {
+            (void)vfxManager;
+            (void)target;
 
-void SpawnCardHeal(BattleResources& resManager, VfxManager& vfxManager, BattleEntity& caster, BattleEntity& target)
-{
-    (void)vfxManager;
-    (void)target;
+            caster.getSprite().setTexture(resManager.CardGuyAtlasTexture());
+            caster.getSprite().setCostume(2);
+        }   
 
-    caster.getSprite().setTexture(resManager.CardGuyAtlasTexture());
-    caster.getSprite().setCostume(2);
-}
+        void SpawnCardBlock(VfxManager& vfxManager, BattleEntity& caster)
+        {
+            (void)vfxManager;
+            (void)caster;
+        }
 
-void SpawnCardBlock(VfxManager& vfxManager, BattleEntity& caster)
-{
-    (void)vfxManager;
-    (void)caster;
-}
-
-} // namespace CardGuy
+    } // namespace CardGuy
 } // namespace AbilityManager
