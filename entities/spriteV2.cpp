e4559d8043eb3092f77281dea44943abc0d85b6a
askar102 <sergey.bolotny@icloud.com>
2026-05-06 16:19:12 +0500

    added SpriteV2 class

---
+++ entities/spriteV2.cpp
@@ -0,0 +1,244 @@
+/**
+ * @file sprite.cpp
+ * @author askar102
+ * @brief Sprite type
+ * @date 2026-05-03
+ * 
+ * @copyright Copyright (c) 2026, askar102
+ * 
+ */
+
+ #include "spriteV2.h"
+
+ bool SpriteV2::_drawHitboxes = false;
+ 
+ /**
+  * 
+  * Resources
+  * 
+  */
+ 
+ void SpriteV2::SetResource(TextureResource* newResource)
+ {
+     _resource = newResource;
+     _frameIndex = 0;
+ 
+     if (!HasResource())
+     {
+         _frame = {0.0f, 0.0f, 0.0f, 0.0f};
+         _size = {0.0f, 0.0f};
+         _rect.width = 0.0f;
+         _rect.height = 0.0f;
+         
+         return;
+     }
+ 
+     if (HasAtlas())
+     {
+         _frame = _resource->frames[0];
+     }
+     else
+     {
+         _frame = {
+             0.0f,
+             0.0f,
+             static_cast<float>(_resource->texture.width),
+             static_cast<float>(_resource->texture.height)
+         };
+     }
+ 
+     SyncSizeWithSource();
+ }
+ 
+ /**
+  * 
+  * Visual
+  * 
+  */
+ 
+ Vector2 SpriteV2::GetPosition() const
+ {
+     return _position;
+ }
+ 
+ void SpriteV2::SetPosition(Vector2 newPosition)
+ {
+     _position = newPosition;
+ }
+ 
+ Vector2 SpriteV2::GetSize() const
+ {
+     return _size;
+ }
+ 
+ void SpriteV2::SetSize(Vector2 newSize)
+ {
+     _size = newSize;
+     _rect.width = _size.x;
+     _rect.height = _size.y;
+ }
+ 
+ float SpriteV2::GetRotation() const
+ {
+     return _rotation;
+ }
+ 
+ void SpriteV2::SetRotation(float newRotation)
+ {
+     _rotation = newRotation;
+ }
+ 
+ float SpriteV2::GetAlpha() const
+ {
+     return _alpha;
+ }
+ 
+ void SpriteV2::SetAlpha(float newAlpha)
+ {
+     _alpha = newAlpha;
+ }
+ 
+ /**
+  * 
+  * Atlas
+  *
+  */
+ 
+ void SpriteV2::SetFrame(size_t index)
+ {
+     if (!HasAtlas())
+     {
+         return;
+     }
+ 
+     if (index >= _resource->frames.size())
+     {
+         return;
+     }
+ 
+     _frameIndex = index;
+     _frame = _resource->frames[_frameIndex];
+     SyncSizeWithSource();
+ }
+ 
+ /**
+  * 
+  * Hitbox visual
+  * 
+  */
+ 
+ void SpriteV2::SetDrawHitboxes(bool value)
+ {
+     _drawHitboxes = value;
+ }
+ 
+ bool SpriteV2::GetDrawHitboxes()
+ {
+     return _drawHitboxes;
+ }
+ 
+ void SpriteV2::SetCanDrawHiboxes(bool canDraw)
+ {
+     _canDrawHitboxes = canDraw;
+ }
+ 
+ /**
+  * 
+  * Hitbox
+  * 
+  */
+ 
+ Rectangle SpriteV2::GetRect() const
+ {
+     return GetWorldRect();
+ }
+ 
+ void SpriteV2::SetRectSize(Vector2 newSize)
+ {
+     _rect.width = newSize.x;
+     _rect.height = newSize.y;
+ }
+ 
+ bool SpriteV2::IsCollide() const
+ {
+     return _collision;
+ }
+ 
+ void SpriteV2::SetCollide(bool isCollide)
+ {
+     _collision = isCollide;
+ }
+ 
+ /**
+  *  
+  * Core
+  * 
+  */
+ 
+  void SpriteV2::Draw()
+  {
+      if (!HasResource())
+          return;
+  
+      Rectangle dest = {_position.x, _position.y, _size.x, _size.y};
+      Vector2 origin = {_size.x * 0.5f, _size.y * 0.5f};
+  
+      DrawTexturePro(_resource->texture, GetSource(), dest, origin, _rotation, Fade(WHITE, _alpha));
+  
+      if (_drawHitboxes && _canDrawHitboxes)
+      {
+          Vector2 rectOrigin = {_rect.width * 0.5f, _rect.height * 0.5f};
+  
+          DrawRectanglePro(
+              {_position.x, _position.y, _rect.width, _rect.height},
+              rectOrigin,
+              _rotation,
+              Fade(WHITE, 0.5f)
+          );
+      }
+  }
+ 
+ /**
+  * 
+  * Misc
+  * 
+  */
+ 
+ bool SpriteV2::HasResource() const
+ {
+     return _resource && _resource->texture.id != 0;
+ }
+ 
+ bool SpriteV2::HasAtlas() const
+ {
+     return HasResource() && !_resource->frames.empty();
+ }
+ 
+ Rectangle SpriteV2::GetSource() const
+ {
+     return _frame;
+ }
+ 
+ void SpriteV2::SyncSizeWithSource()
+ {
+     Rectangle source = GetSource();
+     _size = {source.width, source.height};
+     _rect.width = _size.x;
+     _rect.height = _size.y;
+ }
+ 
+ Rectangle SpriteV2::GetWorldRect() const
+ {
+     return GetWorldRect(_position);
+ }
+ 
+ Rectangle SpriteV2::GetWorldRect(Vector2 position) const
+ {
+     return {
+         position.x - _rect.width * 0.5f,
+         position.y - _rect.height * 0.5f,
+         _rect.width,
+         _rect.height
+     };
+ }
+ 
---
+++ entities/spriteV2.h
@@ -0,0 +1,82 @@
+/**
+ * @file sprite.h
+ * @author askar102
+ * @brief Sprite type
+ * @date 2026-05-03
+ * 
+ * @copyright Copyright (c) 2026, askar102
+ * 
+ */
+
+ #pragma once
+
+ #include "../raylib/raylib.h"
+ #include "../core/resourceManager.h"
+ 
+ class SpriteV2 {
+ private:
+     // resources
+     TextureResource* _resource = nullptr;
+ 
+     // visual
+     Vector2 _position{0.0f, 0.0f};
+     Vector2 _size{0.0f, 0.0f};
+     float _rotation = 0.0f;
+     float _alpha = 1.0f;
+ 
+     // atlas
+     Rectangle _frame{0.0f, 0.0f, 0.0f, 0.0f};
+     size_t _frameIndex = 0;
+ 
+     // hitbox visual
+     static bool _drawHitboxes;
+     bool _canDrawHitboxes = true;
+ 
+     // hitbox
+     Rectangle _rect{_position.x, _position.y, _size.x, _size.y};
+     bool _collision = true;
+ 
+     bool HasResource() const;
+     bool HasAtlas() const;
+     Rectangle GetSource() const;
+     void SyncSizeWithSource();
+ 
+ public:
+     // resources
+     void SetResource(TextureResource* newResource);
+ 
+     // visual
+     Vector2 GetPosition() const;
+     void SetPosition(Vector2 newPosition);
+ 
+     Vector2 GetSize() const;
+     void SetSize(Vector2 newSize);
+ 
+     float GetRotation() const;
+     void SetRotation(float newRotation);
+ 
+     float GetAlpha() const;
+     void SetAlpha(float newAlpha);
+ 
+     // atlas
+     void SetFrame(size_t index);
+ 
+     // hitbox visual
+     static void SetDrawHitboxes(bool value);
+     static bool GetDrawHitboxes();
+ 
+     void SetCanDrawHiboxes(bool canDraw);
+ 
+     // hitbox
+     Rectangle GetRect() const;
+     Rectangle GetWorldRect() const;
+     Rectangle GetWorldRect(Vector2 position) const;
+     void SetRectSize(Vector2 newSize);
+ 
+     bool IsCollide() const;
+     void SetCollide(bool isCollide);
+     
+     // core
+     void Draw();
+ };
+ 
