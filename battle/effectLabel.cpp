cd84415a647624b325a32f41f19be003b6d7bd5e
askar102 <sergey.bolotny@icloud.com>
2026-05-28 10:46:46 +0500

    added: effectLabel animation

--- battle/effectLabel.cpp
+++ battle/effectLabel.cpp
@@ -21,14 +21,14 @@ void EffectLabel::Draw(Vector2 anchorPos, Vector2 anchorSize)
     TextureResource& tex = Game::GetResources().Get(_textureName);
     _sprite.SetResource(&tex);
     _sprite.SetSize({100, 50});
-
-    float labelW = static_cast<float>(tex.texture.width);
+    _sprite.SetAlpha(0.7f);
 
     // fade-out в последние 0.5 секунды
-    float alpha = (_timer < 0.5f) ? (_timer / 0.5f) : 1.0f;
+    float alpha = (_timer < 0.5f) ? (_timer / 0.5f) : 0.7f;
+    float posY = anchorPos.y + _timer * 30.0f;
 
     _sprite.SetAlpha(alpha);
-    _sprite.SetPosition({(anchorPos.x), (anchorPos.y + 100.0f)});
+    _sprite.SetPosition({(anchorPos.x), (posY)});
 
     _sprite.Draw();
 }
