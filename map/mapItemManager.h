39ffe3a0ceffa7490149f376816b467a9e047586
askar102 <sergey.bolotny@icloud.com>
2026-06-17 10:50:33 +0500

    added: itemManager interface

--- map/mapItemManager.h
+++ map/mapItemManager.h
@@ -1,5 +1,17 @@
+#include <vector>
+#include <memory>
+
 #include "mapItem.h"
 
 class MapItemManager {
-    int pizda = 1;
+public:
+    static void SpawnItem(ItemID id);
+    static void SpawnItem(ItemID id, MapLocation loc);
+
+    static void Init(TextureResource* itemIconsPack);
+    static void Draw();
+    static void Update(float dt);
+private:
+    std::vector<std::unique_ptr<MapItem>> _items;
+    TextureResource* _itemIconsPack;
 };
