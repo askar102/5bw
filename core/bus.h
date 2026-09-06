956976c52a5b96a83fb620f57a22790842fa1548
askar102 <askar102@tutamail.com>
2026-09-06 13:28:25 +0500

    fixed: bus

--- core/bus.h
+++ core/bus.h
@@ -23,18 +23,37 @@ public:
      * @param callback A function that is activated upon receiving a message.
      */
     template<typename EventType>
-    void subscribe(std::function<void(const EventType&)> callback);
-
+    inline void subscribe(std::function<void(const EventType&)> callback)
+    {
+        subscribers_[typeid(EventType)].push_back([callback](std::any msg) {
+            callback(std::any_cast<const EventType&>(msg));
+        });
+    }
     /**
      * @brief Method for publish events of a specific type with specified parameters.
      * @tparam EventType Specific event type.
      * @param msg Constructor of EventType.
      */
     template<typename EventType>
-    void publish(const EventType& msg);
+    inline void publish(const EventType& msg)
+    {
+        auto it = subscribers_.find(typeid(EventType));
+        if (it != subscribers_.end()) {
+            for (const auto& callback : it->second) {
+                callback(msg);
+            }
+        }
+    }
+    /**
+     * @brief Clears the content of subscribers container.
+     */
+    inline void reset() noexcept
+    {
+        subscribers_.clear();
+    }
 
 private:
-    std::map<std::type_index, std::vector<std::function<void(std::any)>>> subscribers;
+    std::map<std::type_index, std::vector<std::function<void(std::any)>>> subscribers_;
 };
 
 #endif
